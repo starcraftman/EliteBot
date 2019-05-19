74a74ecb8518eb48c8039cdcc42e33598b71abbb
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2019-05-19 14:53:15 -0400

    Remove Discord ID.

--- extras/init.sql
+++ extras/init.sql
@@ -1,5 +1,6 @@
 -- First admin must be inserted into db, adds all others.
-insert into admins (id, date) VALUES ("discordID1", NOW());
+-- Replace 'EDITHERE' with your discord ID, obtainable by \@DiscordUsername
+insert into admins (id, date) VALUES ("EDITHERE", NOW());
 
 -- Size of databases, MB
 -- SELECT table_schema AS "Database", SUM(data_length + index_length) / 1024 / 1024 AS "Size (MB)" FROM information_schema.TABLES GROUP BY table_schema;
